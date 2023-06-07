0436a41e1ec57db6d6f7debd03be87a765d18935
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-07 13:57:02 +0800

    使用了一下cookie

--- 练习/JavaEE/JavaEE02/src/com/sdm/controller/LoginServlet.java
+++ 练习/JavaEE/JavaEE02/src/com/sdm/controller/LoginServlet.java
@@ -11,10 +11,7 @@ import com.sdm.service.impl.CustomerServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.util.List;
 
@@ -39,6 +36,9 @@ public class LoginServlet extends HttpServlet {
         }
         List<Customer> all = is.getAll();
         request.setAttribute("all", all);
+
+        Cookie cookie = new Cookie("giao", "giao");
+        response.addCookie(cookie);
         request.getRequestDispatcher("main.jsp").forward(request, response);
     }
 
