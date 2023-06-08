f7231f9052ed0c7b0e3f25262719b14019babe04
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-08 14:41:46 +0800

    讲了监听器

--- 练习/JavaEE/JavaEE02/src/com/sdm/filter/LoginInvalidateFilter.java
+++ 练习/JavaEE/JavaEE02/src/com/sdm/filter/LoginInvalidateFilter.java
@@ -2,16 +2,17 @@ package com.sdm.filter;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
  * Author: tyza66
- * Date: 2023/6/8 10:18
+ * Date: 2023/6/8 10:52
  * Github: https://github.com/tyza66
  **/
 
-@WebFilter(filterName = "ChineseFilter", urlPatterns = "/*")//指定要过滤的资源 /*表示所有资源
-public class ChineseFilter implements Filter {
+@WebFilter(filterName = "LoginInvalidateFilter", urlPatterns = "/*")//指定要过滤的资源 /*表示所有资源
+public class LoginInvalidateFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -22,7 +23,11 @@ public class ChineseFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("过滤器处理内容");
+        //用户没有登录的时候判断请求的url 根据请求的url进行判断 如果这些资源是需要登录才能访问的 那就跳转到登陆界面
         servletRequest.setCharacterEncoding("UTF-8");
+        HttpServletRequest request = (HttpServletRequest)servletRequest;
+        System.out.println(request.getRequestURL());//请求地址
+        System.out.println(request.getRequestURI());//请求资源
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
@@ -32,3 +37,4 @@ public class ChineseFilter implements Filter {
         System.out.println("对象销毁了");
     }
 }
+
---
+++ 练习/JavaEE/JavaEE02/src/com/sdm/listener/VisitCountListener.java
@@ -0,0 +1,57 @@
+package com.sdm.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/8 14:41
+ * Github: https://github.com/tyza66
+ **/
+
+@WebListener
+public class VisitCountListener implements ServletContextListener, HttpSessionListener, HttpSessionAttributeListener {
+
+    public VisitCountListener() {
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        /* This method is called when the servlet context is initialized(when the Web application is deployed). */
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        /* This method is called when the servlet Context is undeployed or Application Server shuts down. */
+    }
+
+    @Override
+    public void sessionCreated(HttpSessionEvent se) {
+        /* Session is created. */
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent se) {
+        /* Session is destroyed. */
+    }
+
+    @Override
+    public void attributeAdded(HttpSessionBindingEvent sbe) {
+        /* This method is called when an attribute is added to a session. */
+    }
+
+    @Override
+    public void attributeRemoved(HttpSessionBindingEvent sbe) {
+        /* This method is called when an attribute is removed from a session. */
+    }
+
+    @Override
+    public void attributeReplaced(HttpSessionBindingEvent sbe) {
+        /* This method is called when an attribute is replaced in a session. */
+    }
+}
