14ae3efb0afec365ec3543c2c035cfd93cb36684
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-13 12:42:26 +0800

    浅改

---
+++ 练习/JavaEE/Ajax_JSONdemo/src/com/icss/util/Pager.java
@@ -0,0 +1,16 @@
+package com.icss.util;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/13 11:28
+ * Github: https://github.com/tyza66
+ **/
+
+public class Pager {
+    private int currPage;
+
+    private int pageSize = 5;
+
+    private int start;
+    
+}
