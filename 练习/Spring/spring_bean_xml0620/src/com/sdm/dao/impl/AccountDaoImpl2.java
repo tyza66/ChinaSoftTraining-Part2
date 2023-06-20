ebe1e898a2ae77216432594ad7e0df55c08c4578
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-20 15:02:17 +0800

    使用q来过滤名字

---
+++ 练习/Spring/spring_bean_xml0620/src/com/sdm/dao/impl/AccountDaoImpl2.java
@@ -0,0 +1,20 @@
+package com.sdm.dao.impl;
+
+import com.sdm.dao.AccountDao;
+import com.sdm.pojo.Dog;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Author: tyza66
+ * Date: 2023/6/20 13:08
+ * Github: https://github.com/tyza66
+ **/
+
+@Repository("accountDao2")
+public class AccountDaoImpl2 implements AccountDao {
+    @Override
+    public Dog getDog() {
+        System.out.println("修沟");
+        return new Dog("gg",10);
+    }
+}
--- 练习/Spring/spring_bean_xml0620/src/com/sdm/service/impl/AccountServiceImpl.java
+++ 练习/Spring/spring_bean_xml0620/src/com/sdm/service/impl/AccountServiceImpl.java
@@ -4,6 +4,7 @@ import com.sdm.dao.AccountDao;
 import com.sdm.dao.impl.AccountDaoImpl;
 import com.sdm.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 public class AccountServiceImpl implements AccountService {
 
     @Autowired //默认按类型注入 如果类型找不到或者找到多个 就用属性名作为id去找
+    @Qualifier("accountDao2")
     private AccountDao accountDao;
 
     public AccountServiceImpl() {
