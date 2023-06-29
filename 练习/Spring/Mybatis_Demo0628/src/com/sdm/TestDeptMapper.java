e15d0c3297b3a658357e8e57ba99c049782330d1
洮羱芝闇 <83948628+tyza66@users.noreply.github.com>
2023-06-29 12:50:06 +0800

    浅更新

--- 练习/Spring/Mybatis_Demo0628/src/com/sdm/TestDeptMapper.java
+++ 练习/Spring/Mybatis_Demo0628/src/com/sdm/TestDeptMapper.java
@@ -50,12 +50,18 @@ public class TestDeptMapper {
         InputStream resourceAsStream = Resources.getResourceAsStream(resource);
         //使用SqlSessionFactoryBuilder加载配置文件创建SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
-        //创建SqlSession对象 自动提交
+        //创建SqlSession对象 自动提交'
+        //这个sqlSession有缓存的作用 就是两次查询一样的东西的时候就会直接从缓存里拿而不是再次查找
         SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        sqlSession.clearCache();//可可以清除缓存
         //获得mapper对象
         EmpMapper mapper = sqlSession.getMapper(EmpMapper.class);
         //在延迟状态下 如果值要员工的信息 那么只会执行第一个查询语句
         System.out.println(mapper.selectEmpStepOne("tom"));
         //如果获取部门名称 才会执行关联查询语句
+
+        //如果使用了二级缓存 是针对于mapper进行操作的多个session也可以使用
+        sqlSession.close();
+        resourceAsStream.close();
     }
 }
